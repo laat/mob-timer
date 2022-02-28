35c7fc4b76506665c9b3e5eb98f21988fd804923
Sigurd Fosseng <sigurd@fosseng.net>
2022-02-28 16:15:08 +0100

    remove debug code

--- src/client/room.ts
+++ src/client/room.ts
@@ -2,8 +2,3 @@ import "./components/current-timer.js";
 import "./components/room-tutorial.js";
 import "./components/room-history.js";
 import "./components/progress-bar.js";
-fetch(location.pathname, {
-  method: "PUT",
-  headers: { "content-type": "application/json" },
-  body: JSON.stringify({ timer: 1, user: "me" }),
-});
